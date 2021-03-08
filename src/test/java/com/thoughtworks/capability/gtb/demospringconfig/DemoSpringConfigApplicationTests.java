67c2dd6464f30ee9aa9bd50989765b6e4ab577c0
shuai hao <shuai.hao@shuaideMacBook-Pro.local>
2021-03-08 11:21:10 +0800

    [haoshuai] feat: cover part of application.properties by ProfiledProperty

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,13 +1,21 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:test.properties")
 class DemoSpringConfigApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private LevelController levelController;
+
+    @Test
+    void shouldProfiledProperty_overridePropertyValues() {
+        Assertions.assertEquals("advanced", levelController.getLevel());
+    }
 
 }
