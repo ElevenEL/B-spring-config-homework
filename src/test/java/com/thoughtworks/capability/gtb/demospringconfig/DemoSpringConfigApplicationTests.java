c6de1e4469c8cea04cd2145a5f82031e40508b0e
shuai hao <shuai.hao@shuaideMacBook-Pro.local>
2021-03-08 10:22:38 +0800

    [haoshuai] feat: cover part of application.properties by SpringBootTestAnnotation

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,13 +1,19 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"levelNumber=0"})
 class DemoSpringConfigApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private LevelController levelController;
+
+    @Test
+    void shouldSpringBootTestAnnotation_overridePropertyValues() {
+        Assertions.assertEquals("basic",levelController.getLevel());
+    }
 
 }
