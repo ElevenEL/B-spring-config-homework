ed5b0c95546f2158cbf243ec4d58dbb65f81958b
shuai hao <shuai.hao@shuaideMacBook-Pro.local>
2021-03-08 10:24:59 +0800

    [haoshuai] feat: cover all application.properties by TestResourceFile

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,13 +1,19 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoSpringConfigApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private LevelController levelController;
+
+    @Test
+    void shouldTestResourceFile_overridePropertyValues() {
+        Assertions.assertEquals("advanced", levelController.getLevel());
+    }
 
 }
